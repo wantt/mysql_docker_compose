793bdb467d8f932477b3fbffc71aaced8e4acaac
wantt <wantt2@qq.com>
2020-05-10 09:54:55 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ MYSQL_ROOT_PASSWORD="your_password"
 sed -i '1c GRANT ALL PRIVILEGES ON *.* TO "'root"'@'"%'" IDENTIFIED BY "'$MYSQL_ROOT_PASSWORD'" WITH GRANT OPTION;' init/init.sql
 sed -i 'N;3 i FLUSH PRIVILEGES;' init/init.sql
 
-#MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD docker-compose up -d mysql
+MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD docker-compose up -d mysql
